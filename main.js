d09451b4fca612029550b33e39d3d2552a93a46a
kirill-churin <kirillchurin07@mail.ru>
2021-10-14 21:30:36 +0300

    ЭЭЭ

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.header__burger').click(function() {
         $('.header__burger, .header__menu, .header__logo').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
