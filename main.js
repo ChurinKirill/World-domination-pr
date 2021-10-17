e79b0e7839227fbf31df186d5590a46d7af94662
kirill-churin <kirillchurin07@mail.ru>
2021-10-17 19:53:14 +0300

    Fixed animations

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
+function toggleBurger() {
+    $('.header__burger, .header__menu, .header__logo').toggleClass('active');
+}
+
+function toggleBodyLock() {
+    $('body').toggleClass('lock');
+}
+
+function toggleAnimation(selector) {
+    $(selector).toggleClass('anim');
+}
+
+function isVisible(tag) {
+    var t = $(tag);
+    var w = $(window);
+    var wt = w.scrollTop();
+    var tt = t.offset().top;
+    var tb = tt + t.height();
+    return ((tb - 1 <= wt + w.height()) && (tt >= wt - 100));
+}
+
+function animate(selector) {
+    $(function () {
+        $(window).scroll(function () {
+            var b = $(selector);
+            if (!b.prop("shown") && isVisible(b)) {
+                b.prop("shown", true);
+                toggleAnimation(selector);
+            }
+        });
+    });
+}
+
 $(document).ready(function() {
     $('.header__burger').click(function() {
-        $('.header__burger, .header__menu, .header__logo').toggleClass('active');
-        $('body').toggleClass('lock');
+        toggleBurger();
+        toggleBodyLock();
     });
 });
 
 $(document).ready(function() {
     $('.header__link').click(function() {
-        $('.header__burger, .header__menu, .header__logo').toggleClass('active');
-        $('body').toggleClass('lock');
+        toggleBurger();
+        toggleBodyLock();
     })
-})
+});
 
-var options = {
-    root: document.querySelector('#scrollArea'),
-    rootMargin: '0px',
-    threshold: 1.0
-}
-var callback = function(entries, observer) {
-    entries.forEach(entry => {
-        entry.time;               // a DOMHightResTimeStamp indicating when the intersection occurred.
-        entry.rootBounds;         // a DOMRectReadOnly for the intersection observer's root.
-        entry.boundingClientRect; // a DOMRectReadOnly for the intersection observer's target.
-        entry.intersectionRect;   // a DOMRectReadOnly for the visible portion of the intersection observer's target.
-        entry.intersectionRatio;  // the number for the ratio of the intersectionRect to the boundingClientRect.
-        entry.target;             // the Element whose intersection with the intersection root changed.
-        entry.isIntersecting;     // intersecting: true or false
-    });
-};
-var observer = new IntersectionObserver(callback, options);
-var target = document.querySelector('.avatar1');
-observer.observe(target);
+animate('.example');
+animate('.avatar1');
+animate('.avatar2');
